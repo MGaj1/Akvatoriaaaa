751870d7d8b80e11543e5dd6d27c93ecf360d02e
admin_2 <admin_2@PC3205>
2024-01-23 11:22:11 +0300

    Добавьте файлы проекта.

---
+++ Akvatoria/ClientRegistr.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Akvatoria
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientRegistr.xaml
+    /// </summary>
+    public partial class ClientRegistr : Window
+    {
+        public ClientRegistr()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Akvatoria/Sotrudnik.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Akvatoria
+{
+    /// <summary>
+    /// Логика взаимодействия для Sotrudnik.xaml
+    /// </summary>
+    public partial class Sotrudnik : Window
+    {
+        public Sotrudnik()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
